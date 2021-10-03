c89f30525918003f339aadd533a2e8424340c880
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-10-03 15:20:38 -0500

    commit tree 4ded0098e53bf05e4afcfc6e36c776616ff1c958

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,35 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
-  # Add routes
-  
+
+  get '/messages' do
+    sorted = Message.all.sort_by { |m| m.created_at }
+    sorted.to_json
+  end 
+
+  post '/messages' do 
+    message = Message.create(
+      body: params[:body],
+      username: params[:username],
+      created_at: params[:created_at],
+      updated_at: params[:updated_at]
+    )
+    message.to_json
+  end
+
+  patch '/messages/:id' do
+    message = Message.find params[:id]
+    message.update(
+      body: params[:body],
+      username: params[:username],
+      created_at: params[:created_at],
+      updated_at: params[:updated_at]
+    )
+    message.to_json
+  end
+
+  delete '/messages/:id' do
+    message = Message.find params[:id]
+    message.destroy
+  end
+
 end
